7d12a835120f1911ada661ef18c31de1aef35868
Brian Lee <lehuuchuong62@gmail.com>
2021-10-27 13:16:05 +0700

    Create Trainer Controller and View Index

---
+++ DemoProject1/Controllers/TrainersController.cs
@@ -0,0 +1,26 @@
+﻿using DemoProject1.Models;
+using DemoProject1.Util;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DemoProject1.Controllers
+{
+    [Authorize(Roles = Role.Trainer)]
+    public class TrainersController : Controller
+    {
+        private ApplicationDbContext _context;
+        public TrainersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Trainers
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var trainerInDb = _context.TrainerDb
+                .SingleOrDefault(t => t.TrainerId == userId);
+            return View(trainerInDb);
+        }
+    }
+}
